a08d198f1d2616aa8fe51d74d31fe3dfc2e72d6c
YaelZamora <ajedrezkasparov@gmail.com>
2024-11-28 07:59:45 -0600

    Initial Commit

---
+++ Clipboard/ClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardApp.swift
+//  Clipboard
+//
+//  Created by Yael Javier Zamora Moreno on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
