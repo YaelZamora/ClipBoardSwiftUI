f5dcfc07c7de86e2ec87430e724639e169d20a27
YaelZamora <ajedrezkasparov@gmail.com>
2024-11-28 10:30:08 -0600

    First Commit

--- Clipboard/ContentView.swift
+++ Clipboard/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var text: String = ""
+    @State private var ButtonText: String = "Copy to clipboard"
+    private let clipboard = UIPasteboard.general
+    @State private var textPasted = UserDefaults.standard.string(forKey: "clip")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("Add some text", text: $text)
+            
+            HStack {
+                Button {
+                    copyToClipboard()
+                } label: {
+                    Label(ButtonText, systemImage: "doc.on.doc.fill")
+                }
+                
+                Spacer()
+                
+                Button {
+                    paste()
+                } label: {
+                    Label("Paste", systemImage: "doc.on.clipboard")
+                }.tint(.orange)
+            }
+            
+            Text(textPasted ?? "")
         }
         .padding()
+    }
+    
+    func paste() {
+        if let string = clipboard.string {
+            textPasted = string
+            UserDefaults.standard.set(string, forKey: "clip")
+        }
+    }
+    
+    func copyToClipboard() {
+        clipboard.string = self.text
+        self.text = ""
+        self.ButtonText = "Copied!"
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            self.ButtonText = "Copy to Clipboard"
+        }
     }
 }
 
